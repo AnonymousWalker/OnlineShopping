870a186da9a0afe61e5f28a4a2335d2e283d5992
HTR06-HoangAnh <HTR06@WINDOWS-JEROM6J>
2018-02-21 12:30:30 +0700

    create 2 button Save + Clear

--- OnlineShopping/Scripts/Custom/UploadImage.js
+++ OnlineShopping/Scripts/Custom/UploadImage.js
@@ -1,4 +1,9 @@
-﻿function readURL(input) {
+﻿
+$("#browserbtn").change(function (e) {
+    readURL(this);
+});
+
+function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
@@ -6,7 +11,11 @@
             $('#image')
                 .attr('src', e.target.result);
         };
-
+        $(".image-preview-filename").val(input.files[0].name);
         reader.readAsDataURL(input.files[0]);
     }
 }
+    //$("#clearbtn").click(function (e) {
+    //    e.preventDefault();
+    //    $("#img").attr('src', "");
+    //});
