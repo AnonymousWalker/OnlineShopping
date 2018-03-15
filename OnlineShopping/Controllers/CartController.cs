979e92035457db29838c01faffb38d4535d66735
HTR06-HoangAnh <HTR06@WINDOWS-JEROM6J>
2018-03-15 11:17:44 +0700

    add cart controller

---
+++ OnlineShopping/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using OnlineShopping.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShopping.Controllers
+{
+    public class CartController : Controller
+    {
+        private ControllerService _service;
+        public CartController(ControllerService service)
+        {
+            _service = service;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
