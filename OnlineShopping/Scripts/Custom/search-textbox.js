5ec701e85fe812270cd5d622426b748b271fcf47
Hoang Anh <hoanganhtran1998@gmail.com>
2018-03-19 22:41:33 +0700

    search product with js

--- OnlineShopping/Scripts/Custom/search-textbox.js
+++ OnlineShopping/Scripts/Custom/search-textbox.js
@@ -2,11 +2,16 @@
     $("#searchtextbox").keyup(function (e) {
         if (e.keyCode == 13) {
             var text = $(this).val();
+            var url = $(this).data("url");
+            window.location.href = url+"/?query="+text;
             $.ajax({
                 url: $(this).data("url"),
                 type: "get",
                 data: {
                     query: text
+                },
+                success: function (result) {
+                 //   window.location = result;
                 }
             });
         }
