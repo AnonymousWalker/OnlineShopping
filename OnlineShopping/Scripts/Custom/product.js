f02620077403702a4b35b386962101557b36e0c3
Hoang Anh <hoanganhtran1998@gmail.com>
2018-04-01 23:01:03 +0700

    overflow for product title (link)

--- OnlineShopping/Scripts/Custom/product.js
+++ OnlineShopping/Scripts/Custom/product.js
@@ -1,10 +1,10 @@
-﻿$(document).ready(function () {
-    var products = $("[name='productlink']");
-    for (var i = 0; i < products.length; i++) {
+﻿//$(document).ready(function () {
+//    var products = $("[name='productlink']");
+//    for (var i = 0; i < products.length; i++) {
 
-        if (products[i].text.length >= 24) {
-            products[i].text = products[i].text.substring(0, 22) +"...";
-        }
-    }
+        //if (products[i].text.length >= 24) {
+        //    products[i].text = products[i].text.substring(0, 22) +"...";
+        //}
+//    }
 
-});
+//});
