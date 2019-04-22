056ecbb8b9d30b0e1cf93ee40cdc47e5bb6bb62d
Hoang Anh <hoanganhtran1998@gmail.com>
2019-04-22 10:50:33 -0500

    css

--- OnlineShopping/Scripts/Custom/login.js
+++ OnlineShopping/Scripts/Custom/login.js
@@ -1,3 +1,13 @@
 ﻿$(document).ready(function () {
     $("#Username").focus();
+    $("#sign-up-btn").click(function (e) {
+        var elem = $("#confirm-password");
+        if (elem.val() !== $("#Password").val()) {
+            e.preventDefault();
+            elem.focus();
+            elem.next().text("Password confirmation does not match");
+        } else {
+            elem.next().text("");
+        }
+    });
 });
